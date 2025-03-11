c9bc7e43ca050c742f336d2bc73e6fd5eb78b603
tejaswiniv27 <119059771+tejaswiniv27@users.noreply.github.com>
2025-03-11 18:15:49 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World How are you\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
